63ec0cba9e03a76e14c662bdaae6f6efddb011ed
Mike Loffland <mloffland@outlook.com>
2015-01-31 14:30:10 -0600

    Using Angular Leaflet Directive's built-in functionality to get current position

--- school-finder-frontend/app/app.js
+++ school-finder-frontend/app/app.js
@@ -1,21 +1,52 @@
-var SCHOOL_FINDER = (function(){
+var sfApp = angular.module('sfApp', ["leaflet-directive"]);
 
-  function init() {
-    console.debug("Started initting app");
+sfApp.controller('mapmain', ['$scope', function ($scope) {
+  $scope.locationfound = false;
 
-    // create module
-    var sfApp = angular.module('sfApp', []);
+  angular.extend($scope, {
+    defaults: {
+      maxZoom: 18
+    },
+    center: {
+      lat: 35.478,
+      lng: -97.519,
+      zoom: 12
+    },
+    tiles: {
+      name: 'Mapbox Tiles',
+      url: 'http://{s}.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+      type: 'xyz',
+      options: {
+        apikey: 'pk.eyJ1IjoibWxvZmZsYW5kIiwiYSI6Ik5leC11NlUifQ.h2UgWXhT5l7zjts894SySw',
+        mapid: 'mloffland.l3746b9b'
+      }
+    }
+  });
 
-    // register services
-    sfApp.service("currentLocationService", SCHOOL_FINDER_CLS.CurrentLocationService);
+  $scope.$on("leafletDirectiveMap.locationfound", function (event) {
+    var eventScopeCenter = event.currentScope.center;
+    $scope.locationfound = true;
+    angular.extend($scope, {
+      markers: {
+        ml: {
+          lat: eventScopeCenter.lat,
+          lng: eventScopeCenter.lng,
+          focus: true,
+          message: "You are here (approximately)",
+          draggable: false
+        }
+      }
 
+    });
+  });
 
-    console.debug("Finished initting app");
-  }
-
-  return {
-    init: init
+  $scope.setPosition = function () {
+    angular.extend($scope, {
+      center: {
+        zoom: 15,
+        autoDiscover: true
+      }
+    });
   };
-}());
 
-SCHOOL_FINDER.init();
+}]);
