6831376ea2da0a74adf107081f5abab3c775676d
Mike Loffland <mloffland@outlook.com>
2015-02-04 11:53:57 -0600

    Debugging test

--- school-finder-frontend/tests/geolocation/geolocation_spec.js
+++ school-finder-frontend/tests/geolocation/geolocation_spec.js
@@ -1,5 +1,3 @@
-
-
 describe('School Finder: geolocationFactory', function () {
 
   var geolocationFactory,
@@ -7,43 +5,65 @@ describe('School Finder: geolocationFactory', function () {
     $window,
     $q,
     backupGeo,
-    options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
+    options;
 
-  beforeEach(
-    module('sfApp')
+  beforeEach(function(){
+      module('sfApp');
+      inject(function(_geolocationFactory_, _$rootScope_, _$window_, _$q_) {
+        geolocationFactory = _geolocationFactory_;
+        $rootScope = _$rootScope_;
+        $window = _$window_;
+        $q = _$q_;
+      });
+      options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+    }
   );
 
-  beforeEach(
-    inject(function(_geolocationFactory_, _$rootScope_, _$window_, _$q_) {
-    geolocationFactory = _geolocationFactory_;
-    $rootScope = _$rootScope_;
-    $window = _$window_;
-    $q = _$q_;
-  }));
-
   describe('Call to getCurrentPosition()', function () {
-    it('should return a promise', function() {
-      var ret = geolocationFactory.getCurrentPosition();
+    it('should return a fulfilled promise', function() {
       return geolocationFactory.getCurrentPosition().should.be.fulfilled;
+    });
+    it('should eventually resolve to a position object with coords property', function() {
+      return expect(geolocationFactory.getCurrentPosition()).to.eventually.have.property("coords")
     });
   });
 
-  describe('If the Geolocation API is not available', function () {
+
+  describe('Call to getCurrentPosition(options)', function () {
+    it('should return a fulfilled promise', function() {
+      return geolocationFactory.getCurrentPosition(options).should.be.fulfilled;
+    });
+    it('should eventually resolve to a position object with coords property', function() {
+      return expect(geolocationFactory.getCurrentPosition(options)).to.eventually.have.property("coords")
+    });
+  });
+
+
+  describe('If the Geolocation API is not available on said browser', function () {
     beforeEach(function(){
       backupGeo = $window.navigator.geolocation;
       delete $window.navigator.geolocation;
     });
-
-    it('it should return an error', function() {
-      return geolocationFactory.getCurrentPosition().should.be.rejected;
-    });
-
     afterEach(function(){
       $window.navigator.geolocation = backupGeo;
+    });
+    it('it should return a rejected promise', function() {
+      //var ret = geolocationFactory.getCurrentPosition();
+      return geolocationFactory.getCurrentPosition().should.be.rejected;
+    });
+  });
+
+
+  describe('If it takes too long for the Geolocation API to figure out the position', function () {
+    beforeEach(function(){
+      options.timeout = 5;
+    });
+    it('it should return a rejected promise', function() {
+      return geolocationFactory.getCurrentPosition(options).should.be.rejected;
     });
   });
 
