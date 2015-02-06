373600c41026a0130690e57627d32a13fc2adafa
Mike Loffland <mloffland@outlook.com>
2015-02-06 17:30:01 -0600

    Refactored testing for geolocationFactory.

--- school-finder-frontend/app/geolocationFactory.js
+++ school-finder-frontend/app/geolocationFactory.js
@@ -1,76 +1,39 @@
 (function () {
   var sfApp = angular.module('sfApp');
 
-  var geolocationFactory = function($rootScope, $window, $q){
+  var geolocationFactory = function ($rootScope, $window, $q) {
     // https://github.com/ninjatronic/ngGeolocation
     function supported() {
       return 'geolocation' in $window.navigator;
     }
 
     var retVal = {
-      getCurrentPosition: function(options) {
+      getCurrentPosition: function (options) {
         var deferred = $q.defer();
-        if(supported()) {
+        if (supported()) {
           $window.navigator.geolocation.getCurrentPosition(
-            function(position) {
-              $rootScope.$apply(function() {
+            function (position) {
+              $rootScope.$apply(function () {
                 retVal.position.coords = position.coords;
                 retVal.position.timestamp = position.timestamp;
                 deferred.resolve(position);
               });
             },
-            function(error) {
-              $rootScope.$apply(function() {
+            function (error) {
+              $rootScope.$apply(function () {
                 deferred.reject({error: error});
               });
             }, options);
         } else {
-          deferred.reject({error: {
-            code: 2,
-            message: 'This web browser does not support HTML5 Geolocation'
-          }});
-        }
-        return deferred.promise;
-      },
-
-      watchPosition: function(options) {
-        if(supported()) {
-          if(!this.watchId) {
-            this.watchId = $window.navigator.geolocation.watchPosition(
-              function(position) {
-                $rootScope.$apply(function() {
-                  retVal.position.coords = position.coords;
-                  retVal.position.timestamp = position.timestamp;
-                  delete retVal.position.error;
-                  $rootScope.$broadcast('geolocation.position.changed', position);
-                });
-              },
-              function(error) {
-                $rootScope.$apply(function() {
-                  retVal.position.error = error;
-                  delete retVal.position.coords;
-                  delete retVal.position.timestamp;
-                  $rootScope.$broadcast('geolocation.position.error', error);
-                });
-              }, options);
-          }
-        } else {
-          retVal.position = {
+          deferred.reject({
             error: {
               code: 2,
               message: 'This web browser does not support HTML5 Geolocation'
             }
-          };
+          });
         }
+        return deferred.promise;
       },
-
-      clearWatch: function() {
-        if(this.watchId) {
-          $window.navigator.geolocation.clearWatch(this.watchId);
-          delete this.watchId;
-        }
-      },
-
       position: {}
     };
 
